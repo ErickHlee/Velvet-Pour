034f5d72046da0e7a6bb52e94282f3609418ec93
ErickHlee <erick.hlee@sptech.school>
2026-03-17 20:39:49 -0300

    style: adiciona o estilo base do site

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">Hello, GSAP</div>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello, GSAP</h1>
+    </div>
   )
 }
 
