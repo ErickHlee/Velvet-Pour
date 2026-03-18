e92478a0c1d4d1e8d1a523e63bb6dc42f23fe88a
ErickHlee <erick.hlee@sptech.school>
2026-03-18 15:32:23 -0300

    feat: adiciona base da página Cocktails

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import { cocktailLists, mockTailLists } from "../../constants";
+import gsap from "gsap";
+
+const Cocktails = () => {
+  return (
+    <section id="cocktails" className="noisy">
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+      />
+
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+      />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails:</h2>
+
+          <ul>
+            {mockTailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <h3>
+                    {country} | {detail}
+                  </h3>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Most popular mocktails:</h2>
+
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="md:me-28">
+                  <h3>{name}</h3>
+                  <h3>
+                    {country} | {detail}
+                  </h3>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cocktails;
