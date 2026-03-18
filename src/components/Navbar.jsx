de4d8e7f01ffa4d47a83720c92795461f647f4bb
ErickHlee <erick.hlee@sptech.school>
2026-03-18 10:34:05 -0300

    feat: implmenta navbar e animação

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,45 @@
-import {navLinks} from '../../constants'
+import { useGSAP } from '@gsap/react'
+import { navLinks } from '../../constants'
+import gsap from 'gsap'
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div>
-            <a href="#home" className='flex items-center gap-2'>
-                <p>Velvet Pour</p>
-            </a>
+    useGSAP(() => {
+        // de "inBetween" é o processo de gerar imagem que vai entre os keyFrames (início e fim do frame);
+        const navTween = gsap.timeline({
+            ScrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
 
-            <ul>
-                {navLinks.map((link) => (
-                    <li key={link.id}>
-                        <a href={`#${link.id}`}>{link.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    </nav>
-  )
+        navTween.fromTo('nav',
+            { backgroundColor: 'trnasparent' },
+            {
+                backgroundColor: '#00000050', backgroundFilter: 'blur(10px)',
+                duration: 1,
+                ease: 'power1.inOut',
+            });
+    });
+
+
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex justify-around items-center gap-2'>
+                    <img src="/images/logo.png" alt="logo" />
+                    <p>Velvet Pour</p>
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
